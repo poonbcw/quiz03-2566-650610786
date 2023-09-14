16e3b590b05499408acbc1612ae4b9876027a867
Poon Boonchoowit <poon15112@gmail.com>
2023-09-14 15:13:20 +0700

    completed

--- src/app/api/message/route.js
+++ src/app/api/message/route.js
@@ -5,59 +5,87 @@ import { NextResponse } from "next/server";
 
 export const GET = async (request) => {
   readDB();
+  const roomId = request.nextUrl.searchParams.get("roomId");
+  const foundroom = DB.rooms.find((room) => room.roomId === roomId);
+  if (!foundroom) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: `Room is not found`,
+      },
+      { status: 404 }
+    );
+  }
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: `Room is not found`,
-  //   },
-  //   { status: 404 }
-  // );
+  const messageList = [];
+  for (const message of DB.messages) {
+    if (message.roomId === roomId) messageList.push(message);
+  }
+
+  return NextResponse.json({
+    ok: true,
+    messageList,
+  });
 };
 
 export const POST = async (request) => {
   readDB();
-
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: `Room is not found`,
-  //   },
-  //   { status: 404 }
-  // );
+  const body = await request.json();
+  const { roomId, messageText } = body;
+  const foundroom = DB.rooms.find((room) => room.roomId === roomId);
+  if (!foundroom) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: `Room is not found`,
+      },
+      { status: 404 }
+    );
+  }
 
   const messageId = nanoid();
+  DB.messages.push({
+    roomId,
+    messageId,
+    messageText,
+  });
 
   writeDB();
 
   return NextResponse.json({
     ok: true,
-    // messageId,
+    messageId,
     message: "Message has been sent",
   });
 };
 
 export const DELETE = async (request) => {
   const payload = checkToken();
-
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Invalid token",
-  //   },
-  //   { status: 401 }
-  // );
+  let role = payload.role;
+  if (!payload || role !== "SUPER_ADMIN") {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Invalid token",
+      },
+      { status: 401 }
+    );
+  }
 
   readDB();
-
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Message is not found",
-  //   },
-  //   { status: 404 }
-  // );
-
+  const body = await request.json();
+  const { messageId } = body;
+  const foundIndex = DB.messages.findIndex((x) => x.messageId === messageId);
+  if (foundIndex === -1) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Message is not found",
+      },
+      { status: 404 }
+    );
+  }
+  DB.messages.splice(foundIndex, 1);
   writeDB();
 
   return NextResponse.json({
--- src/app/api/room/route.js
+++ src/app/api/room/route.js
@@ -5,42 +5,54 @@ import { NextResponse } from "next/server";
 
 export const GET = async () => {
   readDB();
+  let count = 0;
+  for (let key in DB.rooms) ++count;
   return NextResponse.json({
     ok: true,
-    //rooms:
-    //totalRooms:
+    rooms: DB.rooms,
+    totalRooms: count,
   });
 };
 
 export const POST = async (request) => {
   const payload = checkToken();
+  if (!payload) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Invalid token",
+      },
+      { status: 401 }
+    );
+  }
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Invalid token",
-  //   },
-  //   { status: 401 }
-  // );
+  const body = await request.json();
+  const { roomName } = body;
 
   readDB();
-
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: `Room ${"replace this with room name"} already exists`,
-  //   },
-  //   { status: 400 }
-  // );
+  const foundRoom = DB.rooms.find((room) => room.roomName === roomName);
+  if (foundRoom) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: `Room ${roomName} already exists`,
+      },
+      { status: 400 }
+    );
+  }
 
   const roomId = nanoid();
 
   //call writeDB after modifying Database
+  DB.rooms.push({
+    roomId,
+    roomName,
+  });
   writeDB();
 
   return NextResponse.json({
     ok: true,
-    //roomId,
-    message: `Room ${"replace this with room name"} has been created`,
+    roomId,
+    message: `Room ${roomName} has been created`,
   });
 };
